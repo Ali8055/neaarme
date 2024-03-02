ea72fdf651da07786592d1724cfe1f938ea1fda2
Ali8055 <muhammadaliawan@gmail.com>
2024-03-02 12:58:00 +0500

    resolving build issues

--- app/(pages)/products/page.jsx
+++ app/(pages)/products/page.jsx
@@ -1,13 +1,9 @@
 "use client";
-import Main from "@/app/components/allProducts/main";
+// import Main from "@/app/components/allProducts/main";
 import React from "react";
 
 function allproducts() {
-  return (
-    <div className="max-w-screen-xl mx-auto">
-      <Main />
-    </div>
-  );
+  return <div className="max-w-screen-xl mx-auto">{/* <Main /> */}</div>;
 }
 
 export default allproducts;
